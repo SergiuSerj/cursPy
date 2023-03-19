972461c23926851332392d14f5d1a63bc2af8d87
SergiuSerj <sergiuhirja@gmail.com>
2023-03-19 20:09:12 +0200

    versiunea2

--- Django_Project/Radiologie/aplicatie1/views.py
+++ Django_Project/Radiologie/aplicatie1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from aplicatie1.models import Location
 
-# Create your views here.
+
+class LocationsView(ListView):
+    model = Location
+    template_name = 'aplicatie1/locations_index.html'
