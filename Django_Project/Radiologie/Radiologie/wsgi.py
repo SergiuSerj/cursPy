7b6b03a6eee0a2af35af4504552b50bed3f75bb4
SergiuSerj <sergiuhirja@gmail.com>
2023-03-19 14:50:28 +0200

    adaugare1

---
+++ Django_Project/Radiologie/Radiologie/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Radiologie project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Radiologie.settings')
+
+application = get_wsgi_application()
